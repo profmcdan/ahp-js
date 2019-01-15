b53424e7347103f273747487132fb6f16f500907
Daniel Ale <danielale9291@gmail.com>
2019-01-15 21:33:57 +0100

    chore/fixed package.json file on both

--- server.js
+++ server.js
@@ -30,35 +30,6 @@ app.use("/api/auth", users);
 
 const PORT = process.env.PORT || 5000;
 
-const AHP = require("ahp");
-var ahpContext = new AHP();
-
-ahpContext.addItems([ "VendorA", "VendorB", "VendorC" ]);
-
-ahpContext.addCriteria([ "price", "functionality", "UX" ]);
-
-//rank criteria with rank scale
-ahpContext.rankCriteriaItem("price", [
-	[ "VendorB", "VendorC", 1 / 2 ],
-	[ "VendorA", "VendorC", 1 / 2 ],
-	[ "VendorA", "VendorB", 1 ]
-]);
-
-//rank criteria with rank scale
-ahpContext.rankCriteriaItem("functionality", [
-	[ "VendorB", "VendorC", 1 ],
-	[ "VendorA", "VendorC", 5 ],
-	[ "VendorA", "VendorB", 5 ]
-]);
-
-//rank criteria with absolute rank scole
-ahpContext.setCriteriaItemRankByGivenScores("UX", [ 10, 10, 1 ]);
-
-ahpContext.rankCriteria([ [ "price", "functionality", 3 ], [ "price", "UX", 3 ], [ "functionality", "UX", 1 ] ]);
-
-let output = ahpContext.run();
-console.log(output);
-
 app.listen(PORT, () => {
 	console.log(`Server is running on PORT ${PORT}`);
 });
