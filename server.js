08cf2fa921ea5e6a882a3da2ea849861f7e066a0
Daniel Ale <danielale9291@gmail.com>
2019-01-20 09:24:46 +0100

    Publish to heroku

--- server.js
+++ server.js
@@ -2,8 +2,21 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const formData = require("express-form-data");
+const cors = require("cors");
+const path = require("path");
+const logger = require("morgan");
 
 const config = require("./db");
+
+app.use(logger("dev"));
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(formData.parse());
+app.use(cors());
 
 const usersRoute = require("./routes/user");
 const bidsRoute = require("./routes/bid");
@@ -38,6 +51,10 @@ app.use("/api/auth", usersRoute);
 app.use("/api/bid", bidsRoute);
 app.use("/api/alternative", contractorRoute);
 app.use("/api/decision", decisionRoute);
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 
