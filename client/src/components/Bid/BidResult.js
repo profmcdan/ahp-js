3620a1043664c87ef663bceb61e688d794610607
Daniel Ale <denielale9291@gmail.com>
2019-03-24 00:43:32 +0100

    Display the matrices on the frontend

--- client/src/components/Bid/BidResult.js
+++ client/src/components/Bid/BidResult.js
@@ -3,11 +3,18 @@ import _ from "lodash";
 import { Header, Button, Checkbox, Icon, Table } from "semantic-ui-react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import { Fraction } from "fractional";
+import "./result.css";
 
 class BidResult extends Component {
   state = {
     bid_id: "",
-    decisions: null
+    decisions: null,
+    user: null,
+    criteria_matrix: [],
+    sub_criteria_matrix: [],
+    alternative_matrix: [],
   };
   getBidDetails = () => {
     const { bid_id } = this.state;
@@ -16,7 +23,7 @@ class BidResult extends Component {
       .then(async res => {
         const { decisions } = res.data;
         await this.setState({
-          decisions
+          decisions,
         });
       })
       .catch(error => {
@@ -25,15 +32,119 @@ class BidResult extends Component {
   };
 
   async componentDidMount() {
-    await this.setState({ bid_id: this.props.match.params.id });
-    await this.getBidDetails();
+    const token = localStorage.getItem("jwtToken");
+    if (token) {
+      const decoded = jwt_decode(token);
+      await this.setState({
+        user: decoded,
+        bid_id: this.props.match.params.id,
+      });
+      console.log(decoded);
+    }
+
+    // await this.getBidDetails();
+    const { user, bid_id } = this.state;
+
+    axios
+      .get(`/api/compute/criteria/${bid_id}/user/${user.id}`)
+      .then(async res => {
+        const {
+          criteria_matrix,
+          sub_criteria_matrix,
+          alternative_matrix,
+        } = res.data;
+        await this.setState({
+          criteria_matrix,
+          sub_criteria_matrix,
+          alternative_matrix,
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
+
+  formatData = valArray => {
+    let stringVal =
+      `${Number.parseFloat(valArray[0]).toFixed(4)}, ` +
+      `${Number.parseFloat(valArray[1]).toFixed(4)}, ` +
+      `${Number.parseFloat(valArray[2]).toFixed(4)}`;
+
+    let stringVal2 =
+      `${new Fraction(valArray[0]).toString()}, ` +
+      `${new Fraction(valArray[1]).toString()}, ` +
+      `${new Fraction(valArray[2]).toString()}`;
+
+    return `(${stringVal})`;
+  };
+
   render() {
-    const { decision, bid_id } = this.state;
+    const {
+      criteria_matrix,
+      bid_id,
+      sub_criteria_matrix,
+      alternative_matrix,
+    } = this.state;
     return (
-      <div>
-        <Header>Bid Result</Header>
-        <p>{bid_id}</p>
+      <div className="main-container">
+        <div>
+          <Header as="h1">Criteria Matrix</Header>
+          <Table striped>
+            <Table.Body>
+              {criteria_matrix.map((critCOl, cIndex) => (
+                <Table.Row key={cIndex}>
+                  {critCOl.map((critRow, rIndex) => (
+                    <Table.Cell key={rIndex}>
+                      {this.formatData(critRow)}
+                    </Table.Cell>
+                  ))}
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </Table>
+        </div>
+        <div>
+          <Header as="h1">SubCriteria Matrix</Header>
+          {sub_criteria_matrix.map((crit, index) => (
+            <div key={index}>
+              <Header as="h3">Criteria {index}</Header>
+              <Table striped>
+                <Table.Body>
+                  {crit.map((critCOl, cIndex) => (
+                    <Table.Row key={cIndex}>
+                      {critCOl.map((critRow, rIndex) => (
+                        <Table.Cell key={rIndex}>
+                          {this.formatData(critRow)}
+                        </Table.Cell>
+                      ))}
+                    </Table.Row>
+                  ))}
+                </Table.Body>
+              </Table>
+            </div>
+          ))}
+        </div>
+        <div>
+          <Header as="h1">Alternatives Matrix</Header>
+          {alternative_matrix.map((sub_crit, index) => (
+            <div key={index}>
+              <Header as="h3">Sub-Criteria {index}</Header>
+              <Table striped>
+                <Table.Body>
+                  {sub_crit.map((critCOl, cIndex) => (
+                    <Table.Row key={cIndex}>
+                      {critCOl.map((critRow, rIndex) => (
+                        <Table.Cell key={rIndex}>
+                          {this.formatData(critRow)}
+                        </Table.Cell>
+                      ))}
+                    </Table.Row>
+                  ))}
+                </Table.Body>
+              </Table>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
